57c70c5543b13c3b4cda324de2bfb07f781ab333
Agfan <91047495+AgfanHerru@users.noreply.github.com>
2021-11-30 23:10:54 +0700

    Create Elemen Kompetensi 1.py

---
+++ Elemen Kompetensi 1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 30 22:26:27 2021
+
+@author: hp
+NAMA: AGFAN HERRU PRATAMA 
+NIM: 064002100043
+"""
+
+print ("@@@@ @@@@ @@@@ @@@@ @@   @")
+print ("@  @ @    @    @  @ @ @  @")
+print ("@@@@ @ @@ @@@@ @@@@ @  @ @")
+print ("@  @ @@@@ @    @  @ @   @@\n")
+
+print('=========== ELKOM 1 ===========')
+list=[1,2,4,3,1,44]
+print(list)
+print('hasil reverse ke tuple')
+print(tuple(list))
