6d2e3a8984adb12653c9e05e2d9d54ee76a00ecd
Agfan <91047495+AgfanHerru@users.noreply.github.com>
2021-11-30 23:08:29 +0700

    Create Elemen Kompetensi 4.py

---
+++ Elemen Kompetensi 4.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 30 22:50:17 2021
+
+@author: hp
+NAMA: AGFAN HERRU PRATAMA HENDARSIN
+NIM: 064002100043
+"""
+print ("@@@@ @@@@ @@@@ @@@@ @@   @")
+print ("@  @ @    @    @  @ @ @  @")
+print ("@@@@ @ @@ @@@@ @@@@ @  @ @")
+print ("@  @ @@@@ @    @  @ @   @@\n")
+
+print("----- ELKOM 4 -----")
+
+print(["z","xvc","cac",244])
+print('- cac')
+print('-244')
+print('Terdapat 2 string yang memenuhi syarat')
