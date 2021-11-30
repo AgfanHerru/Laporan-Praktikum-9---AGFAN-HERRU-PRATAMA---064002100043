c8b884b13b77c8d9d121653937e9b4bc0b18c147
Agfan <91047495+AgfanHerru@users.noreply.github.com>
2021-11-30 23:09:46 +0700

    Create Elemen Kompetensi 3.py

---
+++ Elemen Kompetensi 3.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 30 22:40:42 2021
+
+@author: hp
+NAMA: AGFAN HERRU PRATAMA HENDARSIN
+NIM: 0640002100043
+"""
+
+print ("@@@@ @@@@ @@@@ @@@@ @@   @")
+print ("@  @ @    @    @  @ @ @  @")
+print ("@@@@ @ @@ @@@@ @@@@ @  @ @")
+print ("@  @ @@@@ @    @  @ @   @@\n")
+
+print ("ELKOM 3")
+
+
+def mengalikan(a):
+    hitungan=1
+    for n in list:
+        hitungan*=n
+    print(hitungan)
+    return hitungan
+
+list=[1,2,4,3]
+print(list)
+mengalikan(list)
